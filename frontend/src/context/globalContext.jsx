7b0b314bf8951208eb6c38b815915075db0c8a5d
Iffat1406 <iffatshaikh1406@gmail.com>
2025-03-12 14:41:39 +0530

    Update globalContext.jsx

--- frontend/src/context/globalContext.jsx
+++ frontend/src/context/globalContext.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "finance-visualizer-api.vercel.app/api/v1/";
+const BASE_URL = "https://finance-visualizer-api.vercel.app/api/v1/";
 
 
 const GlobalContext = React.createContext()
